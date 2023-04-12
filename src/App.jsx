bd1b5d038a35abefbeb9f86cbb0a46cf0db95720
Vivian Zhang <vvynz12@gmail.com>
2023-04-11 23:09:48 -0400

    Create a setup a List component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+
+import { List } from "./components";
+
+import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
       <h1>NYC Trip Planner</h1>
-       
+      <List />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
