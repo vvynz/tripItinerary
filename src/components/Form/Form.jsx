69be579a27afa3791809b245c4d269c67add1000
Vivian Zhang <vvynz12@gmail.com>
2023-04-15 19:18:02 -0400

    Create a setFormChange function that will save the user input to the form data state

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -6,24 +6,50 @@ export default function Form({
   formData,
   setFormData,
 }) {
-
   const onSubmit = (e) => {
     e.preventDefault();
 
     const newItem = {
       id: 1,
-      thingsToDo: formData.thingsToDo
-    }
+      thingsToDo: formData.thingsToDo,
+    };
 
     const newItems = [...listItems, newItem];
 
     setListItems(newItems);
-  }
-  
+
+    setFormData({
+      thingsToDo: ""
+    })
+  };
+
+  const setFormChange = (e) => {
+    e.preventDefault();
+
+    const { name, value } = e.target;
+
+    const newData = { ...formData };
+
+    newData[name] = value;
+
+    setFormData(newData);
+  };
+
+  console.log("list item=", listItems);
+  console.log("formData=", formData);
+
   return (
-    <div>
-      <input type="text" value={formData.thingsToDo} placeholder="things to do..." name="thingsToDo" />
-      <button type="submit" onSubmit={onSubmit}>Add</button>
-    </div>
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        value={formData.thingsToDo}
+        onChange={setFormChange}
+        placeholder="things to do..."
+        name="thingsToDo"
+      />
+      <button type="submit">
+        Add
+      </button>
+    </form>
   );
 }
