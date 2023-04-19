c400c26a8e31d9fd80313717ebc4170449132de0
Vivian Zhang <vvynz12@gmail.com>
2023-04-18 23:34:00 -0400

    intialize firebase and a database

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,14 @@
-
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
 // firebase database configurations
 const firebaseConfig = {
   projectId: "tripplanner-d56a0",
   dbURL: "https://tripplanner-d56a0-default-rtdb.firebaseio.com/",
-}
+};
+
+// initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// initialize database
+const database = getDatabase(app);
