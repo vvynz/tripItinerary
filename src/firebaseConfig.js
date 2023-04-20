7030d39f20382e4f9e7f300abf3c183b3b24f70e
Vivian Zhang <vvynz12@gmail.com>
2023-04-19 21:40:45 -0400

    Update firebase configs"

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,18 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// firebase database configurations
+// Your web app's Firebase configuration
 const firebaseConfig = {
+  apiKey: "AIzaSyANGTBhH0r3IZvBZMktlx6HJiLfg0p-0BA",
+  authDomain: "tripplanner-d56a0.firebaseapp.com",
+  databaseURL: "https://tripplanner-d56a0-default-rtdb.firebaseio.com",
   projectId: "tripplanner-d56a0",
-  dbURL: "https://tripplanner-d56a0-default-rtdb.firebaseio.com/",
+  storageBucket: "tripplanner-d56a0.appspot.com",
+  messagingSenderId: "793739850810",
+  appId: "1:793739850810:web:70891cf08a11bc9602f103"
 };
 
 // initialize Firebase
