a6d4cc633aa00ef8a77db2e141e2688d4b7af14b
Vivian Zhang <vvynz12@gmail.com>
2023-05-09 23:09:19 -0400

    Create a setFormChange function in the FoodListForm component

--- src/components/FoodList/FoodListForm.jsx
+++ src/components/FoodList/FoodListForm.jsx
@@ -18,9 +18,26 @@ export function FoodListForm({
       .catch((err) => console.log(err.message));
   };
 
+  const setFormChange = (e) => {
+    e.preventDefault();
+
+    const { name, value } = e.target;
+
+    const newData = { ...foodListFormData };
+
+    newData[name] = value;
+
+    setFoodListFormData(newData);
+  };
+
   return (
     <form onSubmit={onSubmit}>
-      <input type="text" placeholder="things to eat..." name="thingsToEat" />
+      <input
+        type="text"
+        placeholder="things to eat..."
+        onChange={setFormChange}
+        name="thingsToEat"
+      />
       <button type="submit">Add</button>
     </form>
   );
